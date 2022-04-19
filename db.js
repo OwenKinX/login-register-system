9a0e052317ceef5aaa5962f29823d9cc786f5dc9
Anakin <71423193+OwenKinX@users.noreply.github.com>
2022-04-19 15:11:42 +0700

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,11 @@ const mysql = require('mysql2');
 const config = require('./config/config.json');
 
 const conn = mysql.createConnection({
-  uri: config.uri
+  host: config.host,
+  user: config.user,
+  password: config.password,
+  database: config.database,
+  port: config.port
 });
 
 module.exports = conn
